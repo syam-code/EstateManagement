01248783cc2f6505b489fe5031bd8e6413f4e5a9
syam-code <samsu.syamsuddin1@gmail.com>
2020-09-10 22:37:34 +0700

    First Commit

---
+++ app/src/main/java/com/example/estatem/RecordFragment.java
@@ -0,0 +1,28 @@
+package com.example.estatem;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecordFragment extends Fragment {
+
+    private Spinner spinner;
+    private List<String> item;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root =  inflater.inflate(R.layout.fragment_record, container, false);
+        return root;
+    }
+}
